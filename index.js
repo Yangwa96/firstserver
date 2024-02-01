28dc4591deb4800e6c9af724a3af91b780bc6746
Yangwa96 <155144481+Yangwa96@users.noreply.github.com>
2024-02-01 15:47:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const port = process.env.Port;
+const port = process.env.PORT;
 const  handle = (fileName,statuscode,req,res) =>{
 
         fs.readFile(fileName, "utf-8",(err, data) =>{
