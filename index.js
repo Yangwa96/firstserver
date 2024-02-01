5823f98d7c3deacea5eb43cdd43183ec9cbb78cc
Yangwa96 <155144481+Yangwa96@users.noreply.github.com>
2024-02-01 14:56:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const port = process.env.port;
+const  handle = (fileName,statuscode,req,res) =>{
+
+        fs.readFile(fileName, "utf-8",(err, data) =>{
+            if (err){
+                console.log(err);
+            }else{
+                res.writeHead(200, {"content-type":"text/html"});
+                res.write(data);
+                res.end();
+            };
+        });
+    };
+
+const myServer = http.createServer((req,res) =>{
+    if(req.url === "/"){
+        handle("index.html", 200, req, res);
+    }else if(req.url === "/about"){
+        handle("about.html",200, req, res);
+    }else if(req.url === "/contact"){
+        handle("contact.html", 200,req,res);
+    }else{
+        handle("404.html",404, req,res);
+    }
+});
+
+myServer.listen(port, () => {
+    console.log("server is running")
+});
